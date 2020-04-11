e33d4ad9c64872e5ef4c9b26d2ce5acf0190f4d9
zachary <zpyingling9559@gmail.com>
2020-04-11 15:26:02 -0400

    getting the ravelry api to work; aswell as getting the npm package to put information into the placeholders and the front end javascript will add and remove class names to show information

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,15 @@
-var db = require("../models");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/generate", function(req, res) {
+    res.render("generate");
+  });
+
+  app.get("/profile", function(req, res) {
+    res.render("profile");
   });
 
   // Render 404 page for any unmatched routes
