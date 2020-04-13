348516aeb96bf1195f590440fbd06efa7338f338
Marena Linda <marena25@protonmail.com>
2020-04-13 17:08:13 -0400

    this updates the schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,13 @@
 DROP DATABASE IF EXISTS project2_db;
+
 CREATE DATABASE project2_db;
+
+use project2_db;
+
+CREATE TABLE users(
+	id INT AUTO_INCREMENT NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    profilePicUrl VARCHAR(100),
+    PRIMARY KEY (id)
+);
