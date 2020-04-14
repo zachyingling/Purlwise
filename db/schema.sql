86232aa5ca51211206c5974c874b6813dfca6634
Marena Linda <marena25@protonmail.com>
2020-04-13 22:27:25 -0400

    this fixes the schema CLI issues

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,10 @@ CREATE DATABASE project2_db;
 
 use project2_db;
 
-CREATE TABLE users(
-	id INT AUTO_INCREMENT NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    profilePicUrl VARCHAR(100),
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE users(
+-- 	id INT AUTO_INCREMENT NOT NULL,
+--     username VARCHAR(50) NOT NULL,
+--     password VARCHAR(50) NOT NULL,
+--     profilePicUrl VARCHAR(100),
+--     PRIMARY KEY (id)
+-- );
