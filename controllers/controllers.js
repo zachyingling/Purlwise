dbe6b8c4258190c9f447d1eb96e673f69990d5ba
Marena Linda <marena25@protonmail.com>
2020-04-13 22:18:01 -0400

    this is the dumbest fucking thing

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -17,43 +17,11 @@ router.get("/api/signup", function(req, res) {
 });
 
 router.post("/api/patterns", function(req, res) {
-  Pattern.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
+  Pattern.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(
+    result
+  ) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
-  });
-});
-
-router.put("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
   });
 });
 
