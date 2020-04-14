0e6676a5ae4f07d9c4d11dc589215995c8e8bcea
zachary <zpyingling9559@gmail.com>
2020-04-14 02:39:14 -0400

    fixing generate handlebars; adding header to profile handlebars; on passport for the callback url i have it set to heroku app do the deployed version works; changed index.js so it works without the config.js setup

--- models/index.js
+++ models/index.js
@@ -6,6 +6,7 @@ var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
+// Use this if you have a config.js setup with env
 // var config = require(__dirname + "/../config/config.js")[env];
 var db = {};
 
