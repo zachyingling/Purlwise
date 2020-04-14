5bfe8f7880f89cc6de096118decd5f0250b345d4
zachary <zpyingling9559@gmail.com>
2020-04-14 18:27:30 -0400

    changing callback url

--- public/js/passport.js
+++ public/js/passport.js
@@ -30,7 +30,7 @@ passport.use(
     {
       clientID: "1a0c9a6167a639262425",
       clientSecret: "ceabf4a109a5e27f9327c78c04a74e515f443737",
-      callbackURL: "/auth/github/callback"
+      callbackURL: "https://project-2-znnm.herokuapp.com/auth/github/callback"
     },
     function(accessToken, refreshToken, profile, done) {
       var gitUserName = profile.username;
